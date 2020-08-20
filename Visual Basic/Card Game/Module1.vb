fe0e99775c5d998563eb9dc267d2af9f6f0a9bff
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-08-20 15:49:55 +0100

    VB Init

--- Visual Basic/Card Game/Module1.vb
+++ Visual Basic/Card Game/Module1.vb
@@ -1,7 +1,16 @@
 ﻿Module Module1
+    Function Authenticate(name, password)
+        Dim details As String
 
+        details = name & "," & password
+        FileOpen(0, "player_list.csv", OpenMode.Output)
+
+
+
+    End Function
     Sub Main()
+        Console.WriteLine(CurDir)
 
+        Console.Read()
     End Sub
-
 End Module
