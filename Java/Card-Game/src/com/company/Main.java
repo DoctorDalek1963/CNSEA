944bfa5d08c17375f1b2b8a0fc9a6e84aef0236c
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-08-21 13:10:33 +0100

    Added Java

---
+++ Java/Card-Game/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.File;
+import java.io.Scanner;
+
+public class Main {
+
+    static void authenticate(String name, String password) {
+        File deck = new File("deck.txt");
+        Scanner
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(69, 2));
+    }
+
+}
