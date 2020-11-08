b90f79a33ee44460cf71fc4c3a8eb67dec9fafb4
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-11-08 22:37:11 +0000

    Add more comments

--- Java/Card-Game/src/CardGame.java
+++ Java/Card-Game/src/CardGame.java
@@ -36,6 +36,7 @@ public class CardGame {
     static Card p1ActiveCard;
     static Card p2ActiveCard;
 
+    // Create ArrayLists for players' card stacks
     static ArrayList<Card> p1Cards = new ArrayList<>();
     static ArrayList<Card> p2Cards = new ArrayList<>();
 
@@ -47,14 +48,13 @@ public class CardGame {
         String playerDetails = name + "," + password;
         boolean match = false;
 
+        // Search through player_list.csv to find playerDetails
         try {
             File playerList = new File("player_list.csv");
             Scanner playerListReader = new Scanner(playerList);
 
             while (playerListReader.hasNextLine()) {
-                // Search file for a match
                 String fileDetails = playerListReader.nextLine();
-                // If match is found, set flag var
                 if (fileDetails.equals(playerDetails)) {
                     match = true;
                 }
@@ -65,7 +65,7 @@ public class CardGame {
             e.printStackTrace();
         }
 
-        // If no match was found
+        // If no match was found, quit program
         if (!match) {
             System.out.println();
             System.out.println("Sorry, " + name + ", your username or password was incorrect.");
@@ -89,6 +89,7 @@ public class CardGame {
 
         String newPlayer = name + "," + password;
 
+        // Add newPlayer to player_list.csv
         try {
             // True enables append mode
             PrintWriter printWriter = new PrintWriter(new FileWriter("player_list.csv", true));
@@ -106,6 +107,7 @@ public class CardGame {
     }
 
     public static void winHand(String name, ArrayList<Card> cardStack) {
+        // Add active Cards to player's card stack
         cardStack.add(p1ActiveCard);
         cardStack.add(p2ActiveCard);
 
@@ -116,35 +118,34 @@ public class CardGame {
     }
 
     public static void compare(Card card1, Card card2) {
+        // If colours are equal, higher number wins
         if (card1.getColour().equals(card2.getColour())) {
             if (card1.getNumber() > card2.getNumber()) {
                 winHand(p1Name, p1Cards);
-
             } else {
                 winHand(p2Name, p2Cards);
             }
         } else {
+            // If numbers are the same, compare colours to find winner
             String colour = card1.getColour() + " " + card2.getColour();
             String winColour = "";
 
             if (colour.equals("Red Black") | colour.equals("Black Red")) {
                 winColour = "Red";
-
             } else if (colour.equals("Yellow Red") | colour.equals("Red Yellow")) {
                 winColour = "Yellow";
-
             } else if (colour.equals("Black Yellow") | colour.equals("Yellow Black")) {
                 winColour = "Black";
             }
 
+            // Use winColour to find winning player
             if (winColour.equals(card1.getColour())) {
                 winHand(p1Name, p1Cards);
-
             } else if (winColour.equals(card2.getColour())) {
                 winHand(p2Name, p2Cards);
-
             } else {
-                System.out.println("Unhandled winColour exception in CardGame.compare()");
+                // If neither player wins, print error message
+                System.out.println("Unhandled winColour exception in CardGame.compare().");
             }
         }
     }
@@ -156,6 +157,7 @@ public class CardGame {
         System.out.println();
         System.out.println("Press 1 to add a new player. Press enter to log in.");
 
+        // Call addPlayer() if input == "1"
         if (inputScanner.hasNextLine()) {
             if (inputScanner.nextLine().equals("1")) {
                 addPlayer();
@@ -176,7 +178,7 @@ public class CardGame {
         System.out.print("Player 2, please enter your password: ");
         String p2Pass = inputScanner.nextLine();
 
-        // If same account, disallow it
+        // If same account, quit program
         if (p1Name.equals(p2Name)) {
             System.out.println("Sorry, " + p2Name + ", but that's the same account as player 1.");
             System.out.println("Press enter to exit");
@@ -186,8 +188,10 @@ public class CardGame {
 
         authenticate(p2Name, p2Pass);
 
+        // Create ArrayList of Cards to allow for shuffling later
         ArrayList<Card> deckList = new ArrayList<>();
 
+        // Read deck.csv into deckList
         try {
             File deckFile = new File("deck.csv");
             Scanner deckScanner = new Scanner(deckFile);
@@ -202,6 +206,7 @@ public class CardGame {
             e.printStackTrace();
         }
 
+        // Shuffle deckList and create normal array called deck
         Collections.shuffle(deckList);
         Card[] deck = deckList.toArray(new Card[0]);
 
@@ -212,6 +217,7 @@ public class CardGame {
 
         int handNum = 1;
 
+        // Loop over deck and compare top 2 Cards every time
         for (int i = 0; i < deck.length; i = i + 2) {
             System.out.println("Hand: " + handNum);
             p1ActiveCard = deck[i];
@@ -223,6 +229,7 @@ public class CardGame {
             System.out.println("Press enter to continue.");
             inputScanner.nextLine();
 
+            // Compare Cards to find winner
             compare(p1ActiveCard, p2ActiveCard);
 
             handNum++;
@@ -232,10 +239,12 @@ public class CardGame {
         System.out.println("The winner is...");
         inputScanner.nextLine();
 
+        // Create variables for winner
         String winner;
         int winNum;
         ArrayList<Card> winCards;
 
+        // Find winner from length of card stacks
         if (p1Cards.size() > p2Cards.size()) {
             winner = p1Name;
             winNum = p1Cards.size();
@@ -253,12 +262,14 @@ public class CardGame {
         System.out.println("They had these cards:");
         inputScanner.nextLine();
 
+        // Print all Cards
         for (Card card : winCards) {
             System.out.println(card.displayName());
         }
 
         System.out.println();
 
+        // Write score to file
         try {
             // True enables append mode
             PrintWriter printWriter = new PrintWriter(new FileWriter("scores.csv", true));
@@ -270,8 +281,10 @@ public class CardGame {
             e.printStackTrace();
         }
 
+        // Create scoresList as ArrayList to allow for sorting later
         ArrayList<Score> scoresList = new ArrayList<>();
 
+        // Read scores from file into scoresList
         try {
             File scoreFile = new File("scores.csv");
             Scanner scoreScanner = new Scanner(scoreFile);
@@ -294,6 +307,7 @@ public class CardGame {
         System.out.println("These are the high scores:");
         inputScanner.nextLine();
 
+        // Print top 5 high scores if available
         if (scores.length >= 5) {
             for (int i = 0; i < 5; i++) {
                 System.out.println(scores[i].displayName());
