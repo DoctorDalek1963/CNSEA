4f1f3f8787c07031a72aab200db732a303cf9050
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-11-10 19:09:48 +0000

    Init Java Dice Game

---
+++ Java/Dice-Game/src/DiceGame.java
@@ -0,0 +1,22 @@
+//import java.io.*;
+//import java.util.*;
+
+//class Score {
+//    private final String name;
+//    private final int number;
+//
+//    Score(String name, int number) {
+//        this.name = name;
+//        this.number = number;
+//    }
+//
+//    public int getNumber() { return number; }
+//    public String displayName() { return this.name + " - " + this.number; }
+//}
+
+public class DiceGame {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
