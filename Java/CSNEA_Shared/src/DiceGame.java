a2788ccd60f3961157d4c65886c183fc3a7861a5
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-24 15:24:40 +0000

    Refactor DiceGame.java to use the Player class better

--- Java/CSNEA_Shared/src/DiceGame.java
+++ Java/CSNEA_Shared/src/DiceGame.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class DiceGame {
 
-    static int p1Score = 0;
-    static int p2Score = 0;
+//    static int p1Score = 0;
+//    static int p2Score = 0;
+
+    static Player p1;
+    static Player p2;
 
     // Scanner object for reading input
     static Scanner inputScanner = new Scanner(System.in);
@@ -11,11 +14,12 @@ public class DiceGame {
     static Random random = new Random();
 
 
-    public static int diceRoll(String name, int score) {
+    public static int diceRoll(String name) {
         // Generate 2 random numbers from 1-6
         int die1 = random.nextInt(6) + 1;
         int die2 = random.nextInt(6) + 1;
         int both = die1 + die2;
+        int score;
 
         // Check state of dice to determine score
         if (die1 == die2) {
@@ -23,17 +27,17 @@ public class DiceGame {
             System.out.println("That means they get to roll an extra die!");
             int doubleDie = random.nextInt(6) + 1;
             System.out.println("Their extra die rolled a " + doubleDie + "!");
-            score += both + doubleDie;
+            score = both + doubleDie;
 
         } else if (both % 2 == 0) {
             System.out.println(name + " rolled an even number ! (" + both + ")");
             System.out.println("That means they gain 10 points!");
-            score += both + 10;
+            score = both + 10;
 
         } else {
             System.out.println(name + " rolled an odd number ! (" + both + ")");
             System.out.println("That means they lose 5 points!");
-            score += both - 5;
+            score = both - 5;
         }
 
         // Reset score if necessary
@@ -59,12 +63,12 @@ public class DiceGame {
             }
         }
 
-        Player player1 = GameMethods.authenticateReturnPlayer(1);
-        Player player2 = GameMethods.authenticateReturnPlayer(2);
+        p1 = GameMethods.authenticateReturnPlayer(1);
+        p2 = GameMethods.authenticateReturnPlayer(2);
 
         // If same account, quit program
-        if (player1.getName().equals(player2.getName())) {
-            System.out.println("Sorry, " + player2.getName() + ", but that's the same account as player 1.");
+        if (p1.getName().equals(p2.getName())) {
+            System.out.println("Sorry, " + p2.getName() + ", but that's the same account as player 1.");
             System.out.println("Press enter to exit");
             inputScanner.nextLine();
             System.exit(0);
@@ -72,20 +76,20 @@ public class DiceGame {
 
         // Do 5 rounds of dice rolling
         for (int i = 0; i < 5; i++) {
-            p1Score = diceRoll(player1.getName(), p1Score);
-            p2Score = diceRoll(player2.getName(), p2Score);
-            System.out.println(player1.getName() + "'s score is " + p1Score + " and " + player2.getName() + "'s score is " + p2Score);
+            p1.incrementScore(diceRoll(p1.getName()));
+            p2.incrementScore(diceRoll(p2.getName()));
+            System.out.println(p1.getName() + "'s score is " + p1.getScore() + " and " + p2.getName() + "'s score is " + p2.getScore());
             System.out.println("Press enter to continue.");
             inputScanner.nextLine();
             System.out.println();
         }
 
         // Break ties
-        while (p1Score == p2Score) {
+        while (p1.getScore() == p2.getScore()) {
             System.out.println("It's a tie! Let's roll; another die to determine the winner!");
-            p1Score += random.nextInt(6) + 1;
-            p2Score += random.nextInt(6) + 1;
-            System.out.println(player1.getName() + "'s score is " + p1Score + " and " + player2.getName() + "'s score is " + p2Score);
+            p1.incrementScore(random.nextInt(6) + 1);
+            p2.incrementScore(random.nextInt(6) + 1);
+            System.out.println(p1.getName() + "'s score is " + p1.getScore() + " and " + p2.getName() + "'s score is " + p2.getScore());
             System.out.println("That means...");
         }
 
@@ -93,12 +97,12 @@ public class DiceGame {
         int winNum;
 
         // Decide winner
-        if (p1Score > p2Score) {
-            winner = player1.getName();
-            winNum = p1Score;
+        if (p1.getScore() > p2.getScore()) {
+            winner = p1.getName();
+            winNum = p1.getScore();
         } else {
-            winner = player2.getName();
-            winNum = p2Score;
+            winner = p2.getName();
+            winNum = p2.getScore();
         }
 
         System.out.println("The winner is " + winner + "!");
@@ -112,7 +116,7 @@ public class DiceGame {
         System.out.println();
         System.out.println("Press enter to finish.");
         inputScanner.nextLine();
-        System.out.println("Thank you, " + player1.getName() + " and " + player2.getName() + ", for playing The Dice Game!");
+        System.out.println("Thank you, " + p1.getName() + " and " + p2.getName() + ", for playing The Dice Game!");
         inputScanner.nextLine();
     }
 }
