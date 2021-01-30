97366c58789a465b963a96fb937582eb433109f1
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-30 19:49:37 +0000

    Implement most of the remaining methods
    I still need to properly implement editOrder() to allow replacement but I'd probably be best off refactoring the whole program to use a HashMap instead of an ArrayList.
    
    I also need to implement confirmOrder() and that's just writing to a .csv file, so I can do that pretty easily later.

--- Java/A_Level_Pizza_System/src/PizzaSystem.java
+++ Java/A_Level_Pizza_System/src/PizzaSystem.java
@@ -45,34 +45,7 @@ public class PizzaSystem {
             System.out.println((i + 1) + ") " + pizzaToppings[i].displayOption());
         }
 
-        // Ask the user to pick an option and keep looping until their input is valid
-        boolean validInput = false;
-        while (!validInput) {
-            String input = inputScanner.nextLine();
-            // Attempt to parse input as integer
-            try {
-                int inputNum = parseInt(input);
-                // Make sure input is in range
-                if (inputNum > 0 & inputNum < (pizzaToppings.length + 1)) {
-                    validInput = true;
-                    inputNum -= 1;
-                    System.out.println("You have selected: " + pizzaToppings[inputNum].getName());
-                    // Confirm selection
-                    System.out.println("Is this correct?");
-                    if (inputScanner.nextLine().matches("[Yy].*")) {
-                        return pizzaToppings[inputNum];
-                    } else {
-                        System.out.println("Please enter your corrected choice.");
-                        validInput = false; // Set this to false to run the loop again
-                    }
-                } else {
-                    System.out.println("That number is not in the valid range of 1-" + pizzaToppings.length + ". Please try again.");
-                }
-            } catch (NumberFormatException e) { // If input cannot be parsed as integer, try again
-                System.out.println("That is not a number. Please try again.");
-            }
-        }
-        throw new RuntimeException("This exception should never be reached. If it gets thrown, something has gone horribly wrong.");
+        return pizzaToppings[takeIntegerInputForArray(pizzaToppings)];
     }
 
     private static String floatToPrice(float number) {
@@ -105,22 +78,80 @@ public class PizzaSystem {
         System.out.println("That comes to a total of £" + floatToPrice(total));
     }
 
-    // TODO: Finish these methods
+    /**
+     * Take user input as integer corresponding to item in PizzaTopping[] array. Will continue to loop until input is an integer in the range of the array.
+     *
+     * @param array Array of PizzaTopping objects
+     * @return User inputted integer. DOES NOT return array element.
+     */
+    private static int takeIntegerInputForArray(PizzaTopping[] array) {
+        // Ask the user to pick an option and keep looping until their input is valid
+        boolean validInput = false;
+        while (!validInput) {
+            String input = inputScanner.nextLine();
+            // Attempt to parse input as integer
+            try {
+                int inputNum = parseInt(input);
+                // Make sure input is in range
+                if (inputNum > 0 & inputNum < (array.length + 1)) {
+                    validInput = true;
+                    inputNum -= 1;
+                    System.out.println("You have selected: " + array[inputNum].getName());
+                    // Confirm selection
+                    System.out.println("Is this correct?");
+                    if (inputScanner.nextLine().matches("[Yy].*")) {
+                        return inputNum;
+                    } else {
+                        System.out.println("Please enter your corrected choice.");
+                        validInput = false; // Set this to false to run the loop again
+                    }
+                } else {
+                    System.out.println("That number is not in the valid range of 1-" + array.length + ". Please try again.");
+                }
+            } catch (NumberFormatException e) { // If input cannot be parsed as integer, try again
+                System.out.println("That is not a number. Please try again.");
+            }
+        }
+        throw new RuntimeException("This exception should never be reached. If it gets thrown, something has gone horribly wrong.");
+    }
+
+    private static int selectItemFromOrder(String prompt) {
+        System.out.println(prompt);
+        System.out.println();
+
+        for (int i = 0; i < orderItems.size(); i++) {
+            System.out.println((i + 1) + ") " + orderItems.get(i).getName());
+        }
+
+        return takeIntegerInputForArray(orderItems.toArray(new PizzaTopping[0]));
+    }
 
     private static void editOrder() {
-        // show full order with indices of orderItems next to them
-        // allow user to input an index of orderItems
-        // then allow them to choose a new topping in place of that item
+        // TODO: then allow them to choose a new topping in place of that item
+        if (!(orderItems.size() > 0)) {
+            System.out.println("Your order is empty.");
+            return;
+        }
+        int selection = selectItemFromOrder("Please select one item from your order to edit:");
+        PizzaTopping newItem = chooseTopping();
+
+        // TODO: Replace orderItems.get(selection) with newItem
+        // Apparently this would be easier to do with a HashMap, so I'll learn how to do that later
     }
 
     private static void removeItemFromOrder() {
-        // show full order with indices of orderItems next to them
-        // allow user to input an index of orderItems
-        // delete that item of orderItems
+        if (!(orderItems.size() > 0)) {
+            System.out.println("Your order is empty.");
+            return;
+        }
+        int selection = selectItemFromOrder("Please select one item from your order to remove:");
+        String item = orderItems.get(selection).getName();
+        orderItems.remove(selection);
+        System.out.println(item + " has been removed.");
     }
 
     private static boolean confirmOrder() {
-        // write order to file
+        // TODO: write order to file
         return true;
     }
 
