69e2639fa83669b8a778cfddf99e5689867d1658
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-07-05 17:40:02 +0100

    Improve game selection

--- Java/CSNEA_Shared/src/main/org/dyson/games/SelectGame.java
+++ Java/CSNEA_Shared/src/main/org/dyson/games/SelectGame.java
@@ -4,17 +4,23 @@ import java.util.Scanner;
 
 public class SelectGame {
 	public static void main(String[] args) {
-		Scanner inputScanner = new Scanner(System.in);
 		System.out.println("Please select a game:");
 		System.out.println("1) Music Quiz");
 		System.out.println("2) Dice Game");
 		System.out.println("3) Card Game");
 
-		switch (inputScanner.nextLine()) {
+		Scanner inputScanner = new Scanner(System.in);
+		String input = inputScanner.nextLine();
+		System.out.println();
+
+		switch (input) {
 			case "1" -> MusicQuiz.main(args);
 			case "2" -> DiceGame.main(args);
 			case "3" -> CardGame.main(args);
-			default -> System.out.println("Unrecognised choice. Please enter '1', '2', or '3'.");
+			default -> {
+				System.out.println("Unrecognised choice. Please enter '1', '2', or '3'.\n");
+				SelectGame.main(args);
+			}
 		}
 	}
 }
